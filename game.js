2df91a93ffa9d24f36e8580fad80ba1c114c6af7
KainyRyu <KainyRyu@users.noreply.github.com>
2019-12-15 23:12:16 +0000

    some styling and separate js file

---
+++ game.js
@@ -0,0 +1,73 @@
+let recyclable = document.querySelectorAll(".recyclable");
+let general = document.querySelectorAll(".general");
+
+const recycleBin = document.querySelector(".recycleB");
+const generalBin = document.querySelector(".generalB");
+let bins = document.querySelectorAll(".bin");
+let dragged;
+
+function recycleStart(event) {
+  dragged = event.target;
+}
+function dragEnter(event) {
+  if (
+    (dragged.classList.contains("recyclable") &&
+      event.currentTarget.classList.contains("recycleB")) ||
+    (dragged.classList.contains("general") &&
+      event.currentTarget.classList.contains("generalB"))
+  ) {
+    event.currentTarget.classList.add("bin-hover");
+    event.currentTarget.classList.add("open");
+  } else {
+    // wrong();
+    console.log("printingggggggggggggggggggggggggggg and stilllllllllllll");
+  }
+}
+
+function dragOver(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+function dragLeave(event) {
+  event.target.classList.remove("bin-hover");
+  event.target.classList.remove("open");
+}
+function onDrop(event) {
+  console.log("ondrop called!");
+  event.preventDefault();
+  event.currentTarget.classList.remove("bin-hover");
+  event.currentTarget.classList.remove("open");
+  console.log(event.target);
+  recycling(event);
+}
+function initialiseBin() {
+  [recycleBin, generalBin].forEach(bin => {
+    bin.addEventListener("dragenter", dragEnter);
+    bin.addEventListener("dragover", dragOver);
+    bin.addEventListener("dragleave", dragLeave);
+    bin.addEventListener("drop", onDrop);
+  });
+}
+initialiseBin();
+
+function recycling(event) {
+  event.preventDefault();
+  if (
+    dragged.classList.contains("recyclable") &&
+    event.target.classList.contains("recycleB")
+  ) {
+    dragged.remove();
+    initialiseBin();
+  } else if (
+    dragged.classList.contains("general") &&
+    event.target.classList.contains("generalB")
+  ) {
+    dragged.remove();
+    initialiseBin();
+  }
+  event.dataTransfer.clearData; //*************** */
+}
+
+/*=============================================
+                  MOVE AWAY
+==============================================*/
