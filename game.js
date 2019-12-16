65c7c158ce23d001219f09dd518105b47b4098c9
KainyRyu <KainyRyu@users.noreply.github.com>
2019-12-16 14:09:46 +0000

    Mobile friendly Test

--- game.js
+++ game.js
@@ -76,3 +76,41 @@ function recycling(event) {
 function check() {
   document.getElementById("modal-1").checked = true;
 }
+
+function wrong() {
+  for (var i = 0; i < disaster.length; i++) {
+    Math.random * 0.5;
+  }
+}
+
+/*=============================================
+               MOBILE FRIENDLY 
+==============================================*/
+
+window.onload = function() {
+  // find the element that you want to drag.
+  var box = document.getElementById("maindboard");
+
+  /* listen to the touchMove event,
+  every time it fires, grab the location
+  of touch and assign it to box */
+
+  box.addEventListener("touchmove", function(e) {
+    // grab the location of touch
+    var touchLocation = e.targetTouches[0];
+
+    // assign box new coordinates based on the touch.
+    box.style.left = touchLocation.pageX + "px";
+    box.style.top = touchLocation.pageY + "px";
+  });
+
+  /* record the position of the touch
+  when released using touchend event.
+  This will be the drop position. */
+
+  box.addEventListener("touchend", function(e) {
+    // current box position.
+    var x = parseInt(box.style.left);
+    var y = parseInt(box.style.top);
+  });
+};
